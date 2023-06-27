c4fc4139cfadb442067aad47e30c0f668ca19148
M. Faris shafwan <farisbos.mfs@gmail.com>
2023-06-27 19:13:33 +0700

    update

--- app/koneksi.php
+++ app/koneksi.php
@@ -1,6 +1,11 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "admin");
+// $mysqli = new mysqli("localhost", "root", "", "admin");
+$host = '103.175.217.139';
+$dbname = 'sibsdp';
+$user = 'sibsdp';
+$password = 'sibsdp5432';
+$mysqli = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
 
 if (!$mysqli) {
     echo "Koneksi bermasalah !";
